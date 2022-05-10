1229c2d92960328c342b0c02607835dbcf080a58
kifril-ltd <sukhorukov.kirill23@gmail.com>
2022-05-10 17:51:41 +0300

    Добавл DTO для запроса на получение текущего пользователя

---
+++ src/Dto/Response/CurrentUserDto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Dto\Response;
+
+class CurrentUserDto
+{
+
+    public string $username;
+
+    public array $roles;
+
+    public float $balance;
+}
---
+++ src/Dto/Response/Transfromer/CurrentUserResponseTransformer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Dto\Response\Transfromer;
+
+use App\Dto\Response\CurrentUserDto;
+use App\Dto\Response\UserAuthDto;
+use App\Entity\User;
+
+class CurrentUserResponseTransformer
+{
+    public function transformFromObject(User $user): CurrentUserDto
+    {
+        $currentUserDto = new CurrentUserDto();
+
+        $currentUserDto->username = $user->getUsername();
+        $currentUserDto->roles = $user->getRoles();
+        $currentUserDto->balance = $user->getBalance();
+
+        return $currentUserDto;
+    }
+}
